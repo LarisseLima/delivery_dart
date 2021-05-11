29e0cfc7790270b74a902165f2c49822c032db35
Larisse Lima <larisse.lima@matilha.digital>
2021-05-10 21:39:31 -0300

    Getting firebase images

--- lib/tabs/home_tabs.dart
+++ lib/tabs/home_tabs.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
@@ -11,20 +12,50 @@ class HomeTab extends StatelessWidget {
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         );
 
-    return Stack(children: <Widget>[
-      _buildBodyBack(),
-      CustomScrollView(slivers: <Widget>[
-        SliverAppBar(
-          floating: true,
-          snap: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          flexibleSpace: FlexibleSpaceBar(
-            title: const Text('Lima Delivery'),
-            centerTitle: true,
-          ),
-        ),
-      ]),
-    ]);
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('Lima Delivery'),
+                centerTitle: true,
+              ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: Container(),
+                    ),
+                  );
+              },
+            ),
+          ],
+        )
+      ],
+    );
   }
 }

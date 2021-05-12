0d54c3941b4a9a90fcf50e5ef6425f7717911e05
Larisse Lima <larisse.lima@matilha.digital>
2021-05-12 20:30:27 -0300

    Products screen, grid and list

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lima_delivery/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  final String type;
+  final ProductData product;
+
+  ProductTile(this.type, this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

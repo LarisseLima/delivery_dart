fe9c758b02ab742eecdf814ac9f799628066632f
Larisse Lima <larisse.lima@matilha.digital>
2021-05-12 21:19:39 -0300

    Products screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:lima_delivery/datas/product_data.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -7,12 +9,86 @@ class ProductScreen extends StatefulWidget {
   ProductScreen(this.product);
 
   @override
-  _ProductScreenState createState() => _ProductScreenState();
+  _ProductScreenState createState() => _ProductScreenState(product);
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  final ProductData product;
+
+  String size;
+
+  _ProductScreenState(this.product);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: ["images"],
+              // dotSize: 4.0,
+              // dotSpacing: 15.0,
+              // dotBgColor: Colors.transparent,
+              // dotColor: primaryColor,
+              // autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Entre para Comprar",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

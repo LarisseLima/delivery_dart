67fa05bdb18acb52bfb8b2f7936a201b03974ea3
Larisse Lima <larisse.lima@matilha.digital>
2021-05-13 21:43:57 -0300

    Signup  Screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:lima_delivery/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatefulWidget {
+class SignUpScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _SignUpScreenState createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passController = TextEditingController();
+  final _addressController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -18,21 +21,8 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
-          title: Text("Entrar"),
+          title: Text("Criar Conta"),
           centerTitle: true,
-          actions: <Widget>[
-            FlatButton(
-              child: Text(
-                "CRIAR CONTA",
-                style: TextStyle(fontSize: 15.0),
-              ),
-              textColor: Colors.white,
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => SignUpScreen()));
-              },
-            )
-          ],
         ),
         body: ScopedModelDescendant(
           builder: (context, child, model) {
@@ -46,6 +36,16 @@ class _LoginScreenState extends State<LoginScreen> {
               child: ListView(
                 padding: EdgeInsets.all(16.0),
                 children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(hintText: "Nome Completo"),
+                    validator: (text) {
+                      if (text.isEmpty) return "Nome Inválido!";
+                    },
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
                   TextFormField(
                     controller: _emailController,
                     decoration: InputDecoration(hintText: "E-mail"),
@@ -67,32 +67,15 @@ class _LoginScreenState extends State<LoginScreen> {
                         return "Senha inválida!";
                     },
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: FlatButton(
-                      onPressed: () {
-                        if (_emailController.text.isEmpty)
-                          _scaffoldKey.currentState.showSnackBar(SnackBar(
-                            content:
-                                Text("Insira seu e-mail para recuperação!"),
-                            backgroundColor: Colors.redAccent,
-                            duration: Duration(seconds: 2),
-                          ));
-                        else {
-                          model.recoverPass(_emailController.text);
-                          _scaffoldKey.currentState.showSnackBar(SnackBar(
-                            content: Text("Confira seu e-mail!"),
-                            backgroundColor: Theme.of(context).primaryColor,
-                            duration: Duration(seconds: 2),
-                          ));
-                        }
-                      },
-                      child: Text(
-                        "Esqueci minha senha",
-                        textAlign: TextAlign.right,
-                      ),
-                      padding: EdgeInsets.zero,
-                    ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(hintText: "Endereço"),
+                    validator: (text) {
+                      if (text.isEmpty) return "Endereço inválido!";
+                    },
                   ),
                   SizedBox(
                     height: 16.0,
@@ -101,7 +84,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     height: 44.0,
                     child: RaisedButton(
                       child: Text(
-                        "Entrar",
+                        "Criar Conta",
                         style: TextStyle(
                           fontSize: 18.0,
                         ),
@@ -109,7 +92,19 @@ class _LoginScreenState extends State<LoginScreen> {
                       textColor: Colors.white,
                       color: Theme.of(context).primaryColor,
                       onPressed: () {
-                        if (_formKey.currentState.validate()) {}
+                        if (_formKey.currentState.validate()) {
+                          Map<String, dynamic> userData = {
+                            "name": _nameController.text,
+                            "email": _emailController.text,
+                            "address": _addressController.text
+                          };
+
+                          model.signUp(
+                              userData: userData,
+                              pass: _passController.text,
+                              onSuccess: _onSuccess,
+                              onFail: _onFail);
+                        }
                       },
                     ),
                   ),
@@ -118,5 +113,24 @@ class _LoginScreenState extends State<LoginScreen> {
             );
           },
         ));
+  }
+
+  void _onSuccess() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Usuário criado com sucesso!"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 2),
+    ));
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      Navigator.of(context).pop();
+    });
+  }
+
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Falha ao criar usuário!"),
+      backgroundColor: Colors.redAccent,
+      duration: Duration(seconds: 2),
+    ));
   }
 }
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:lima_delivery/screens/login_screen.dart';
 import 'package:lima_delivery/tiles/drawer_tile.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CustomDrawer extends StatelessWidget {
-
   final PageController pageController;
 
   CustomDrawer(this.pageController);
 
   @override
   Widget build(BuildContext context) {
-
     Widget _buildDrawerBack() => Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 203, 236, 241),
-                Colors.white
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter
-          )
-      ),
-    );
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color.fromARGB(255, 203, 236, 241), Colors.white],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter)),
+        );
 
     return Drawer(
       child: Stack(
@@ -40,40 +34,38 @@ class CustomDrawer extends StatelessWidget {
                     Positioned(
                       top: 8.0,
                       left: 0.0,
-                      child: Text("Lima Delivery",
-                        style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+                      child: Text(
+                        "Lima Delivery",
+                        style: TextStyle(
+                            fontSize: 34.0, fontWeight: FontWeight.bold),
                       ),
                     ),
                     Positioned(
                       left: 0.0,
                       bottom: 0.0,
-                      child: ScopedModelDescendant<>(
-                        builder: (context, child, model){
+                      child: ScopedModelDescendant(
+                        builder: (context, child, model) {
                           return Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Text("Olá}",
+                              Text(
+                                "Olá}",
                                 style: TextStyle(
                                     fontSize: 18.0,
-                                    fontWeight: FontWeight.bold
-                                ),
+                                    fontWeight: FontWeight.bold),
                               ),
                               GestureDetector(
                                 child: Text(
-                                  
                                   "Entre ou cadastre-se >",
-                                  
                                   style: TextStyle(
                                       color: Theme.of(context).primaryColor,
                                       fontSize: 16.0,
-                                      fontWeight: FontWeight.bold
-                                  ),
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                onTap: (){
-                                 
-                                    
-                                 
-                                  },
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => LoginScreen()));
+                                },
                               ),
                             ],
                           );
@@ -87,7 +79,8 @@ class CustomDrawer extends StatelessWidget {
               DrawerTile(Icons.home, "Início", pageController, 0),
               DrawerTile(Icons.list, "Produtos", pageController, 1),
               DrawerTile(Icons.location_on, "Lojas", pageController, 2),
-              DrawerTile(Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
+              DrawerTile(
+                  Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
             ],
           ),
         ],
